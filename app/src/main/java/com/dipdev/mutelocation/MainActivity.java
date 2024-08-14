0b633242e3b846d7e7e14d7b82e70f113d9db318
Dipankaj Singh <dipankajsingh2@.com>
2024-08-14 22:07:26 +0530

    get location done

--- app/src/main/java/com/dipdev/mutelocation/MainActivity.java
+++ app/src/main/java/com/dipdev/mutelocation/MainActivity.java
@@ -1,24 +1,113 @@
 package com.dipdev.mutelocation;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private final static int REQUEST_CODE = 100;
+    private LocationManager locationManager;
+    private TextView longitudeTextView;
+    private TextView latitudeTextView;
+    private TextView addressTextView;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button buttonTest = findViewById(R.id.buttonTest);
+        longitudeTextView = findViewById(R.id.longituteTextView);
+        latitudeTextView = findViewById(R.id.latituteTextView);
+        addressTextView = findViewById(R.id.addressTextView);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        buttonTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLastKnownLocation();
+            }
         });
+    }
+
+    private void getLastKnownLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProviderClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null)
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+
+                        @SuppressLint("SetTextI18n")
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                try {
+                                    Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+                                    List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                                    longitudeTextView.setText("Longitude: " + String.valueOf(location.getLongitude()));
+                                    latitudeTextView.setText("Latitude: " + String.valueOf(location.getLatitude()));
+                                    assert addresses != null;
+                                    addressTextView.setText("Address: " + addresses.get(0).getAddressLine(0));
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                    Toast.makeText(MainActivity.this, "Something went wrong, "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                    });
+        } else {
+            askPermission();
+        }
+    }
+
+    private void askPermission() {
+        ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
+    }
+
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull @org.jetbrains.annotations.NotNull String[] permissions, @NonNull @org.jetbrains.annotations.NotNull int[] grantResults) {
+
+        if (requestCode == REQUEST_CODE){
+
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+
+                getLastKnownLocation();
+
+            }else {
+
+
+                Toast.makeText(MainActivity.this,"Please provide the required permission",Toast.LENGTH_SHORT).show();
+
+            }
+
+
+
+        }
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
